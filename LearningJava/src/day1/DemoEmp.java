246e157922abe863b0087b5127aff7868de86e59
ishank <ishank@192.168.1.9>
2020-10-25 08:34:30 +0530

    day2

---
+++ LearningJava/src/day1/DemoEmp.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class DemoEmp {
+
+	public static void main(String[] args) {
+		Employee nikhil=new Employee();
+		nikhil.salary=14000;
+		nikhil.bonus=10000;
+		nikhil.calculateSal();
+
+	}
+
+}
